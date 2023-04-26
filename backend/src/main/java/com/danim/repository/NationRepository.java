69805c8b07c471a54e515c6f274a47c2a6e731d0
leejiyeon <sky75013@naver.com>
2023-04-26 16:56:11 +0900

    [BE] fix: entity 자료형 수정

--- backend/src/main/java/com/danim/repository/NationRepository.java
+++ backend/src/main/java/com/danim/repository/NationRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NationRepository extends JpaRepository<Nation, Long> {
+public interface NationRepository extends JpaRepository<Nation, Integer> {
     Nation findFirstByName(String address1);
 
 }
