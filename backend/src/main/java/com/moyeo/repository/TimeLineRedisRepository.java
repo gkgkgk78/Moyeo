68a3228e9732cb01ec9691b2d7516ffaca50459d
leejiyeon <sky75013@naver.com>
2023-05-01 14:03:01 +0900

    [BE] chore: 'backend' 프로젝트 삭제

--- backend/src/main/java/com/moyeo/repository/TimeLineRedisRepository.java
+++
@@ -1,9 +0,0 @@
-package com.moyeo.repository;
-
-import com.moyeo.entity.RedisPage;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TimeLineRedisRepository extends CrudRepository<RedisPage, Integer> {
-}
--- backend/src/main/java/com/moyeo/service/UtilService.java
+++
@@ -1,8 +0,0 @@
-package com.moyeo.service;
-
-import java.time.LocalDateTime;
-
-public interface UtilService {
-
-    String invertLocalDate(LocalDateTime time);
-}
