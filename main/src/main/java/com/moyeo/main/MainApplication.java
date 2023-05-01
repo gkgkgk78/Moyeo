59cc8cc2c0078fbe78476f6ec5ab6bc7847d4c55
leejiyeon <sky75013@naver.com>
2023-05-01 12:50:34 +0900

    [BE] chore: 프로젝트 옮기기

--- main/src/main/java/com/moyeo/main/MainApplication.java
+++ main/src/main/java/com/moyeo/main/MainApplication.java
@@ -1,10 +1,21 @@
 package com.moyeo.main;
 
+import java.util.Locale;
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainApplication {
+	// 서버 실행 전 아시아/서울 시간으로 서버 시간 동기화
+	@PostConstruct
+	public void started() {
+		Locale.setDefault(Locale.KOREA);
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
