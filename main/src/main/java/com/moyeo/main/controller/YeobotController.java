bb86c2aa4d84832258efcadb16422905e8aea256
신희수 <star_of_arabia@nate.com>
2023-05-02 10:26:54 +0900

    [BE] fix : 여봇 로직 수정 및 완료

--- main/src/main/java/com/moyeo/main/controller/YeobotController.java
+++ main/src/main/java/com/moyeo/main/controller/YeobotController.java
@@ -3,16 +3,18 @@ package com.moyeo.main.controller;
 
 import com.moyeo.main.dto.TravelRecommendRequest;
 import com.moyeo.main.entity.User;
+import com.moyeo.main.repository.UserRepository;
 import com.moyeo.main.service.YeobotService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/auth/yeobot")
@@ -21,58 +23,90 @@ import org.springframework.web.bind.annotation.RestController;
 public class YeobotController {
 
     private final YeobotService yeobotService;
+    private final UserRepository userRepository;
+
+    /*
+    *
+    * 로그인 기능 정상작동 이후에 로그인 로직으로 변경 예정
+    * -> @requestparam userid 삭제 및 주석처리된 로그인 정보에서 uid받아오는 로직으로 변경
+    *
+    * */
 
     //유저가 여행중인지 여부 반환
     @PostMapping("/istravelling")
-    public ResponseEntity<String> getLatestTimelineStatus() throws Exception{
-
+    public ResponseEntity<String> getLatestTimelineStatus(@RequestParam("userId") Long userId) throws Exception{
+        User user = userRepository.findById(userId).orElseThrow(() -> new Exception());
         //로그인 정보에서 uid 받아오기
-        Long userId = null;
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if(auth != null && auth.getPrincipal() != null) {
-            User user = (User) auth.getPrincipal();
-            userId = user.getUserId();
-        }
+//        Long userId = null;
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        if(auth != null && auth.getPrincipal() != null) {
+//            User user = (User) auth.getPrincipal();
+//            userId = user.getUserId();
+//        }
 
         //여행중 여부 반환
-        String status = yeobotService.getLatestTimelineStatus(userId);
+        String status = yeobotService.getLatestTimelineStatus(user.getUserId());
+        System.out.println("여긴가3");
         return ResponseEntity.ok(status);
     }
 
     //여행중인 유저 맛집 추천
     @PostMapping("/ing/dining")
-    public ResponseEntity<String> restaurantRecommendations() throws Exception{
+    public ResponseEntity<String> restaurantRecommendations(@RequestParam Long userId) throws Exception{
         //로그인 정보에서 uid 받아오기
-        Long userId = null;
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if(auth != null && auth.getPrincipal() != null) {
-            User user = (User) auth.getPrincipal();
-            userId = user.getUserId();
-        }
+//        Long userId = null;
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        if(auth != null && auth.getPrincipal() != null) {
+//            User user = (User) auth.getPrincipal();
+//            userId = user.getUserId();
+//        }
         // 최신 주소 반환
-        String[] latestAddress = yeobotService.findLatestAddress(userId);
+        List<String[]> latestAddress = yeobotService.findLatestAddress(userId);
 
         // 프롬프트 반환
-        String goal = "Search for a good restaurant near " + latestAddress[0] + " " + latestAddress[1] + " " + latestAddress[2] + " " + latestAddress[3];
+//        String[] trimmedAddress = Arrays.stream(latestAddress)
+//                .map(String::trim)
+//                .toArray(String[]::new);
+//
+//        String joinedAddress = String.join(" ", trimmedAddress);
+//        String goal = "Search for a good restaurant near " + joinedAddress.replaceAll(",", " ") + ".";
+        List<String> addressList = new ArrayList<>();
 
+        for (String[] addresses : latestAddress) {
+            for (String address : addresses) {
+                addressList.add(String.valueOf(address));
+            }
+        }
+
+        String goal = "Search for a good restaurant near " + addressList.get(0) +" "+ addressList.get(1) +" "+ addressList.get(2) +" "+ addressList.get(3) +".";
         return ResponseEntity.ok(goal);
     }
 
     //여행중인 유저 액티비티 추천
     @PostMapping("/ing/activity")
-    public ResponseEntity<String> activityRecommendationsForTraveller() throws Exception{
+    public ResponseEntity<String> activityRecommendationsForTraveller(@RequestParam Long userId) throws Exception{
         //로그인 정보에서 uid 받아오기
-        Long userId = null;
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if(auth != null && auth.getPrincipal() != null) {
-            User user = (User) auth.getPrincipal();
-            userId = user.getUserId();
-        }
+//        Long userId = null;
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        if(auth != null && auth.getPrincipal() != null) {
+//            User user = (User) auth.getPrincipal();
+//            userId = user.getUserId();
+//        }
         // 최신 주소 반환
-        String[] latestAddress = yeobotService.findLatestAddress(userId);
+        List<String[]> latestAddress = yeobotService.findLatestAddress(userId);
+
+        List<String> addressList = new ArrayList<>();
+
+        for (String[] addresses : latestAddress) {
+            for (String address : addresses) {
+                addressList.add(String.valueOf(address));
+            }
+        }
 
         // 프롬프트 반환
-        String goal = "Recommend me some fun things to do near " + latestAddress[0] + " " + latestAddress[1] + " " + latestAddress[2] + " today.";
+        String goal = "Recommend me some fun things to do near "+ addressList.get(0) + " "
+                + addressList.get(1) + " "
+                + addressList.get(2) + " today.";
 
         return ResponseEntity.ok(goal);
     }
