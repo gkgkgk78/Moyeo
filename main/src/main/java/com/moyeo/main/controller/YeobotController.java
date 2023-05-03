d9d561b7f6fa2a72f1901df562bd9d9d06ce1546
신희수 <star_of_arabia@nate.com>
2023-05-03 09:54:32 +0900

    [BE] feat : 여봇 controller 로직 로그인 거치도록 수정, okhttp 적용

--- main/src/main/java/com/moyeo/main/controller/YeobotController.java
+++ main/src/main/java/com/moyeo/main/controller/YeobotController.java
@@ -1,6 +1,7 @@
 package com.moyeo.main.controller;
 
 
+import com.moyeo.main.conponent.YeobotClient;
 import com.moyeo.main.dto.TravelRecommendRequest;
 import com.moyeo.main.entity.User;
 import com.moyeo.main.repository.UserRepository;
@@ -12,6 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -23,53 +25,46 @@ import java.util.List;
 public class YeobotController {
 
     private final YeobotService yeobotService;
-    private final UserRepository userRepository;
+ //   private final UserRepository userRepository;
+    private final YeobotClient yeobotClient;
 
-    /*
-    *
-    * 로그인 기능 정상작동 이후에 로그인 로직으로 변경 예정
-    * -> @requestparam userid 삭제 및 주석처리된 로그인 정보에서 uid받아오는 로직으로 변경
-    *
-    * */
+//    public YeobotController(YeobotService yeobotService, YeobotClient yeobotClient) {
+//        this.yeobotService = yeobotService;
+//        this.yeobotClient = yeobotClient;
+//    }
+    
+    //테스트 하려면 flask서버도 켜놔야함
 
     //유저가 여행중인지 여부 반환
     @PostMapping("/istravelling")
-    public ResponseEntity<String> getLatestTimelineStatus(@RequestParam("userId") Long userId) throws Exception{
-        User user = userRepository.findById(userId).orElseThrow(() -> new Exception());
+    public ResponseEntity<String> getLatestTimelineStatus(){
         //로그인 정보에서 uid 받아오기
-//        Long userId = null;
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        if(auth != null && auth.getPrincipal() != null) {
-//            User user = (User) auth.getPrincipal();
-//            userId = user.getUserId();
-//        }
+        Long userId = null;
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth != null && auth.getPrincipal() != null) {
+            User user = (User) auth.getPrincipal();
+            userId = user.getUserId();
+        }
 
         //여행중 여부 반환
-        String status = yeobotService.getLatestTimelineStatus(user.getUserId());
-        System.out.println("여긴가3");
+        String status = yeobotService.getLatestTimelineStatus(userId);
         return ResponseEntity.ok(status);
     }
 
     //여행중인 유저 맛집 추천
     @PostMapping("/ing/dining")
-    public ResponseEntity<String> restaurantRecommendations(@RequestParam Long userId) throws Exception{
+    public ResponseEntity<String> restaurantRecommendations() throws Exception {
         //로그인 정보에서 uid 받아오기
-//        Long userId = null;
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        if(auth != null && auth.getPrincipal() != null) {
-//            User user = (User) auth.getPrincipal();
-//            userId = user.getUserId();
-//        }
+        Long userId = null;
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth != null && auth.getPrincipal() != null) {
+            User user = (User) auth.getPrincipal();
+            userId = user.getUserId();
+        }
         // 최신 주소 반환
         List<String[]> latestAddress = yeobotService.findLatestAddress(userId);
 
         // 프롬프트 반환
-//        String[] trimmedAddress = Arrays.stream(latestAddress)
-//                .map(String::trim)
-//                .toArray(String[]::new);
-//
-//        String joinedAddress = String.join(" ", trimmedAddress);
-//        String goal = "Search for a good restaurant near " + joinedAddress.replaceAll(",", " ") + ".";
         List<String> addressList = new ArrayList<>();
 
         for (String[] addresses : latestAddress) {
@@ -79,19 +74,27 @@ public class YeobotController {
         }
 
         String goal = "Search for a good restaurant near " + addressList.get(0) +" "+ addressList.get(1) +" "+ addressList.get(2) +" "+ addressList.get(3) +".";
-        return ResponseEntity.ok(goal);
+        //return ResponseEntity.ok(goal);
+
+        ResponseEntity<String> response = ResponseEntity.ok(goal);
+
+        // Flask 서버에 데이터 전송
+        yeobotClient.sendYeobotData("ing/dining", goal);
+
+        return response;
+
     }
 
     //여행중인 유저 액티비티 추천
     @PostMapping("/ing/activity")
-    public ResponseEntity<String> activityRecommendationsForTraveller(@RequestParam Long userId) throws Exception{
+    public ResponseEntity<String> activityRecommendationsForTraveller() throws Exception{
         //로그인 정보에서 uid 받아오기
-//        Long userId = null;
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        if(auth != null && auth.getPrincipal() != null) {
-//            User user = (User) auth.getPrincipal();
-//            userId = user.getUserId();
-//        }
+        Long userId = null;
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth != null && auth.getPrincipal() != null) {
+            User user = (User) auth.getPrincipal();
+            userId = user.getUserId();
+        }
         // 최신 주소 반환
         List<String[]> latestAddress = yeobotService.findLatestAddress(userId);
 
@@ -108,12 +111,20 @@ public class YeobotController {
                 + addressList.get(1) + " "
                 + addressList.get(2) + " today.";
 
-        return ResponseEntity.ok(goal);
+        //return ResponseEntity.ok(goal);
+
+        ResponseEntity<String> response = ResponseEntity.ok(goal);
+
+        // Flask 서버에 데이터 전송
+        yeobotClient.sendYeobotData("ing/activity", goal);
+
+        return response;
+
     }
 
     //여행중이 아닌 유저 여행지 추천
     @PostMapping("/yet/place")
-    public ResponseEntity<String> recommendPlace(@RequestBody TravelRecommendRequest request) {
+    public ResponseEntity<String> recommendPlace(@RequestBody TravelRecommendRequest request) throws IOException {
         // request에서 필요한 정보를 추출해서 변수에 저장
         String destination = request.getDestination();
         String season = request.getSeason();
@@ -123,13 +134,20 @@ public class YeobotController {
         String goal = "Recommend me a good place for travel to go in " + destination + " in " + season + " for " + purpose +".";
 
         // 프롬프트 반환
-        return ResponseEntity.ok(goal);
+        //return ResponseEntity.ok(goal);
+
+        ResponseEntity<String> response = ResponseEntity.ok(goal);
+
+        // Flask 서버에 데이터 전송
+        yeobotClient.sendYeobotData("yet/place", goal);
+
+        return response;
     }
 
 
     //여행중이 아닌 유저 액티비티 추천
     @PostMapping("/yet/activity")
-    public ResponseEntity<String> recommendActivities(@RequestBody TravelRecommendRequest request) {
+    public ResponseEntity<String> recommendActivities(@RequestBody TravelRecommendRequest request) throws IOException {
         // request에서 필요한 정보를 추출해서 변수에 저장
         String destination = request.getDestination();
         String season = request.getSeason();
@@ -138,7 +156,14 @@ public class YeobotController {
         String goal = "Recommend me some fun things to do near " + destination + " during " + season +".";
 
         // 프롬프트 반환
-        return ResponseEntity.ok(goal);
+        //return ResponseEntity.ok(goal);
+
+        ResponseEntity<String> response = ResponseEntity.ok(goal);
+
+        // Flask 서버에 데이터 전송
+        yeobotClient.sendYeobotData("yet/activity", goal);
+
+        return response;
 
     }
 
