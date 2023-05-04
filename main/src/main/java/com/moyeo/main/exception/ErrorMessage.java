a2d60466260789db52ad2fec952dcfd6fef20203
woojin <woojin1home@naver.com>
2023-05-04 16:23:31 +0900

    [BE]fix : firebase 관련 push 확인

--- main/src/main/java/com/moyeo/main/exception/ErrorMessage.java
+++ main/src/main/java/com/moyeo/main/exception/ErrorMessage.java
@@ -34,6 +34,10 @@ public enum ErrorMessage {
     NOT_EXIST_PHOTO("존재하지 않는 사진 입니다", HttpStatus.BAD_REQUEST),
 
     TOO_LONG_COMMAND("input 명령어가 너무 깁니다.", HttpStatus.BAD_REQUEST),
+
+    FIREBASE_INIT_ERROR("firebase 초기화시 에러 발생", HttpStatus.INTERNAL_SERVER_ERROR),
+
+    FIREBASE_SEND_ERROR("firebase 알림 메시지 전송중 에러", HttpStatus.INTERNAL_SERVER_ERROR),
     NOT_EXIST_LATEST_ADDRESS("최근 주소 정보가 존재하지 않습니다.", HttpStatus.NOT_FOUND);
 
     private final String errMsg;
