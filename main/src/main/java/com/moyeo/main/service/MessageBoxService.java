112d361b586e546214d71c50acb76980ea0a9bf3
신희수 <star_of_arabia@nate.com>
2023-05-06 00:54:04 +0900

    [BE] feat : MessageBox 로직 1차 완료

--- main/src/main/java/com/moyeo/main/service/MessageBoxService.java
+++ main/src/main/java/com/moyeo/main/service/MessageBoxService.java
@@ -1,4 +1,17 @@
 package com.moyeo.main.service;
 
+import com.moyeo.main.dto.MyMessageBoxDTO;
+
+import java.util.List;
+
 public interface MessageBoxService {
+
+    public List<MyMessageBoxDTO> getMessagesByUser(Long userId);
+
+    public void markAsCheckedByMessageId(Long messageId);
+
+    public void markAsCheckedAll(Long userId);
+
+    public void RemoveMessage(Long messageId);
+
 }
