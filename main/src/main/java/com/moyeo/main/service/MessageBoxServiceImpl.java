72dd905de82f0241e884fcf7b4f87f700e46c935
신희수 <star_of_arabia@nate.com>
2023-05-06 00:54:04 +0900

    [BE] feat : MessageBox 로직 1차 완료

--- main/src/main/java/com/moyeo/main/service/MessageBoxServiceImpl.java
+++ main/src/main/java/com/moyeo/main/service/MessageBoxServiceImpl.java
@@ -1,9 +1,15 @@
 package com.moyeo.main.service;
 
+import com.moyeo.main.dto.MyMessageBoxDTO;
+import com.moyeo.main.entity.MessageBox;
 import com.moyeo.main.repository.MessageBoxRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +17,35 @@ import org.springframework.stereotype.Service;
 public class MessageBoxServiceImpl implements MessageBoxService{
 
     private final MessageBoxRepository messageBoxRepository;
+
+    public List<MyMessageBoxDTO> getMessagesByUser(Long userId) {
+        List<MessageBox> messages = messageBoxRepository.findByUserIdOrderByIsCheckedAscCreateTimeDesc(userId);
+        List<MyMessageBoxDTO> MyMessageDtos = new ArrayList<>();
+        for (MessageBox message : messages) {
+            MyMessageDtos.add(MyMessageBoxDTO.builder()
+                    .messageId(message.getMessageId())
+                    .content(message.getContent())
+                    .isChecked(message.getIsChecked())
+                    .createTime(message.getCreateTime())
+                    .build());
+        }
+        return MyMessageDtos;
+    }
+
+    @Transactional
+    public void markAsCheckedByMessageId(Long messageId) {
+        messageBoxRepository.markAsCheckedById(messageId);
+    }
+
+    @Override
+    public void markAsCheckedAll(Long userId) {
+        messageBoxRepository.markAsCheckedByUser(userId);
+    }
+
+    @Override
+    public void RemoveMessage(Long messageId) {
+        messageBoxRepository.deleteByMessageId(messageId);
+    }
+
 
 }
