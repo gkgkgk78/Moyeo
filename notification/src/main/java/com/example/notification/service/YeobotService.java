71343d485901b602d87aa2097ac3b7f8594edb6f
정우진 <woojin1home@naver.com>
2023-05-06 21:08:10 +0900

    [BE] fix : 현재 비동기로 동작은 하나, autogpt에게 내린 명령에 대한 답변이 잘 안오는중

---
+++ notification/src/main/java/com/example/notification/service/YeobotService.java
@@ -0,0 +1,10 @@
+package com.example.notification.service;
+
+import java.util.List;
+
+public interface YeobotService {
+
+    public List<String[]> findLatestAddress(Long userId) throws Exception;
+
+
+}
---
+++ notification/src/main/java/com/example/notification/service/YeobotServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.notification.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class YeobotServiceImpl implements YeobotService {
+
+
+    public List<String[]> findLatestAddress(Long userId) {
+
+
+        return null;
+
+    }
+
+
+}
