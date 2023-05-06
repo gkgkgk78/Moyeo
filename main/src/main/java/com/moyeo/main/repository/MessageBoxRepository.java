5788e8ed22d8a3991bacfc445e0227c6f105d951
신희수 <star_of_arabia@nate.com>
2023-05-06 15:32:20 +0900

    [BE] fix : MessageBox 로직 save 제외 테스트 및 에러수정 완료

--- main/src/main/java/com/moyeo/main/repository/MessageBoxRepository.java
+++ main/src/main/java/com/moyeo/main/repository/MessageBoxRepository.java
@@ -1,22 +1,27 @@
 package com.moyeo.main.repository;
 
 import com.moyeo.main.entity.MessageBox;
+import com.moyeo.main.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
+@Repository
 public interface MessageBoxRepository extends JpaRepository<MessageBox, Long> {
 
-    List<MessageBox> findByUserIdOrderByIsCheckedAscCreateTimeDesc(Long userId);
+    List<MessageBox> findByUserIdOrderByIsCheckedAscCreateTimeDesc(User userId);
 
     @Modifying
-    @Query("UPDATE MessageBox m SET m.isChecked = true WHERE m.messageId = :messageId")
+    @Transactional
+    @Query("update MessageBox set isChecked = true where messageId = :messageId")
     void markAsCheckedById(Long messageId);
 
     @Modifying
-    @Query("UPDATE MessageBox m SET m.isChecked = true WHERE m.userId = :userId")
+    @Transactional
+    @Query("UPDATE MessageBox m SET m.isChecked = true WHERE m.userId.userId = :userId")
     void markAsCheckedByUser(Long userId);
 
     void deleteByMessageId(Long messageId);
--- main/src/main/java/com/moyeo/main/service/MessageBoxServiceImpl.java
+++ main/src/main/java/com/moyeo/main/service/MessageBoxServiceImpl.java
@@ -2,14 +2,18 @@ package com.moyeo.main.service;
 
 import com.moyeo.main.dto.MyMessageBoxDTO;
 import com.moyeo.main.entity.MessageBox;
+import com.moyeo.main.entity.User;
 import com.moyeo.main.repository.MessageBoxRepository;
+import com.moyeo.main.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,9 +21,14 @@ import java.util.List;
 public class MessageBoxServiceImpl implements MessageBoxService{
 
     private final MessageBoxRepository messageBoxRepository;
+    private final UserRepository userRepository;
 
+    @Override
     public List<MyMessageBoxDTO> getMessagesByUser(Long userId) {
-        List<MessageBox> messages = messageBoxRepository.findByUserIdOrderByIsCheckedAscCreateTimeDesc(userId);
+        //테스트 편의를 위한 설정. 로그인 이후 필요 없을시 삭제
+        User usertmp = userRepository.getByUserId(userId);
+        List<MessageBox> messages = messageBoxRepository.findByUserIdOrderByIsCheckedAscCreateTimeDesc(usertmp);
+        //List<MessageBox> messages = messageBoxRepository.findByUserIdOrderByIsCheckedAscCreateTimeDesc(userId);
         List<MyMessageBoxDTO> MyMessageDtos = new ArrayList<>();
         for (MessageBox message : messages) {
             MyMessageDtos.add(MyMessageBoxDTO.builder()
@@ -32,17 +41,51 @@ public class MessageBoxServiceImpl implements MessageBoxService{
         return MyMessageDtos;
     }
 
+    @Override
     @Transactional
     public void markAsCheckedByMessageId(Long messageId) {
+
+        if (messageId == null) {
+            throw new IllegalArgumentException("messageId cannot be null");
+        }
+
+        Optional<MessageBox> messageBoxOptional = messageBoxRepository.findById(messageId);
+        if (messageBoxOptional.isEmpty()) {
+            throw new RuntimeException("No message found for the given messageId");
+        }
+
         messageBoxRepository.markAsCheckedById(messageId);
     }
 
     @Override
+    @Transactional
     public void markAsCheckedAll(Long userId) {
+        if (userId == null) {
+            throw new IllegalArgumentException("userId cannot be null");
+        }
+
+        User user = userRepository.getByUserId(userId);
+        List<MessageBox> messageBoxList = messageBoxRepository.findByUserIdOrderByIsCheckedAscCreateTimeDesc(user);
+//        if (messageBoxList == null || messageBoxList.isEmpty()) {
+//            throw new RuntimeException("No messages found for the user");
+//        }
+//        messageBoxRepository.markAsCheckedByUser(userId);
+
+        if (messageBoxList == null || messageBoxList.isEmpty()) {
+            throw new RuntimeException("No messages found for the user");
+        }
+
         messageBoxRepository.markAsCheckedByUser(userId);
+
+//        for (MessageBox messageBox : messageBoxList) {
+//            messageBox.setIsChecked(true);
+//        }
+//
+//        messageBoxRepository.saveAll(messageBoxList);
     }
 
     @Override
+    @Transactional
     public void RemoveMessage(Long messageId) {
         messageBoxRepository.deleteByMessageId(messageId);
     }
