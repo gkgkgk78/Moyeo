de96d141ce5e64ff570470be2ef96e7d9be8bbd4
Jul <lnacles@naver.com>
2023-05-09 17:29:27 +0900

    Feat:batch컨트롤러 매핑 수정

--- notification/src/main/java/com/example/notification/controller/BathchController.java
+++ notification/src/main/java/com/example/notification/controller/BathchController.java
@@ -5,8 +5,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RequestMapping("/batch")
 @RequiredArgsConstructor
@@ -15,7 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class BathchController {
     private final FcmService fcmService;
     @PostMapping(value = "/push")
-    ResponseEntity<?> pushNotification(String deviceToken, String message) throws Exception {
+    ResponseEntity<?> pushNotification(@RequestBody Map<String,String> map) throws Exception {
+        String deviceToken = map.get("deviceToken");
+        String message = map.get("message");
         log.info("Batch Restaurant Reco TO :{}",deviceToken);
         log.info("Batch Restaurant Reco MESSAGE :{}",message);
         String result = fcmService.pushNoti(deviceToken,message);
