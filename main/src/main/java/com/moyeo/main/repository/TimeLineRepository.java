dcb7cdba3a8bce8d34502b2b9ee13f6a7f604f5e
leejiyeon <sky75013@naver.com>
2023-05-09 15:51:05 +0900

    [BE] fix: 여행 중인지 체크 로직 수정

--- main/src/main/java/com/moyeo/main/repository/TimeLineRepository.java
+++ main/src/main/java/com/moyeo/main/repository/TimeLineRepository.java
@@ -18,6 +18,7 @@ import java.util.Optional;
 //mapper와 동일한 기능을 제공한다고 생각을 하자
 @Repository
 public interface TimeLineRepository extends JpaRepository<TimeLine, Long> {
+    Optional<TimeLine> findFirstByUserId(User user);
     Optional<TimeLine> findFirstByUserIdAndIsComplete(User user, Boolean isComplete);
 
     @Override
