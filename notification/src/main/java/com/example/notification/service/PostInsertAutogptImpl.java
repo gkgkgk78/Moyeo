f05b7617cde598eb0a166f00665cebe963ff4287
woojin <woojin1home@naver.com>
2023-05-10 14:36:04 +0900

    [BE] fix : post insert 시 push 작업 추가

--- notification/src/main/java/com/example/notification/service/PostInsertAutogptImpl.java
+++ notification/src/main/java/com/example/notification/service/PostInsertAutogptImpl.java
@@ -22,30 +22,46 @@ public class PostInsertAutogptImpl implements PostInsertAutogpt {
 
     private final ChatService chatService;
 
+    /*
+    * 맛집
+        String goal = "Search for a good restaurant near " + addressList.get(0) +" "+ addressList.get(1) +" "+ addressList.get(2) +" "+ addressList.get(3) +".";
+        String caseType = "restaurant";
+
+
+        액티비티
+        String goal = "Recommend me some fun things to do near "+ addressList.get(0) + " "+ addressList.get(1) + " " + addressList.get(2) + " today.";
+        String caseType = "activity";
+
+    ** */
+
     @Override
     public void insert(PostInsertReq post) {
 
         //해당 요청이 들어오면, 액티비티, 여행지, 맛집 세개를 추천을 해줘야 함
         //autogpt에게 응답을 보낸후
-        String goal = "Search for a good restaurant near " + post.getAddress1() + " " + post.getAddress2() + " " + post.getAddress3() + " " + post.getAddress4() + ".";
+        String res = "Search for a good restaurant near " + post.getAddress1() + " " + post.getAddress2() + " " + post.getAddress3() + " " + post.getAddress4() + ".";
+        String act = "Recommend me some fun things to do near " + post.getAddress1() + " " + post.getAddress2() + " " + post.getAddress3() + " today.";
+        String goal1[] = {res, act};
+        String goal2[] = {"restaurant", "activity"};
+
 
         try {
 
-            String a1 = yeobotClient.sendYeobotData("restaurant", goal);
-            log.info("autogpt에게 응답 받음 " + a1.toString());
-            System.out.println(a1.toString());
-            //받은 응답을 바탕으로 푸시 알림을 해줘야 함
-            fcmService.send(post.getDeviceToken(), a1);
-            User user = userRepository.getByUserId(post.getUserId());
-            Chat chat = Chat.builder(a1).build();
-            chatService.insertAutogpt(post.getUserId().toString(), chat);
-
+            for (int i=0;i<2;i++){
+                String a1 = yeobotClient.sendYeobotData(goal2[i], goal1[i]);
+                log.info("autogpt에게 응답 받음 " + a1.toString());
+                //System.out.println(a1.toString());
+                //받은 응답을 바탕으로 푸시 알림을 해줘야 함
+                fcmService.send(post.getDeviceToken(), a1);
+                User user = userRepository.getByUserId(post.getUserId());
+                Chat chat = Chat.builder(a1).build();
+                chatService.insertAutogpt(post.getUserId().toString(), chat);
+            }
 
         } catch (Exception e) {
             log.info(e.getMessage());
             return;
         }
-
 
     }
 
