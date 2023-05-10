0c79474125bef5355e486228e3464e605ca22d52
신희수 <star_of_arabia@nate.com>
2023-05-10 17:15:55 +0900

    [BE] fix : 여봇 예외처리 추가 : 시간정보가 없는경우

--- main/src/main/java/com/moyeo/main/exception/ErrorMessage.java
+++ main/src/main/java/com/moyeo/main/exception/ErrorMessage.java
@@ -50,6 +50,7 @@ public enum ErrorMessage {
     FIREBASE_SEND_ERROR("firebase 알림 메시지 전송중 에러", HttpStatus.INTERNAL_SERVER_ERROR),
 
     NOT_EXIST_LATEST_ADDRESS("최근 주소 정보가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+    NOT_EXIST_LATEST_TIME("최근 시간 정보가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
     FLASK_SERVER_ERROR("플라스크 서버 연결에 문제가 있습니다.", HttpStatus.BAD_REQUEST),
 
     NO_MESSAGE_FOR_USER("해당 유저가 수신한 메시지가 존재하지 않습니다.", HttpStatus.BAD_REQUEST),
