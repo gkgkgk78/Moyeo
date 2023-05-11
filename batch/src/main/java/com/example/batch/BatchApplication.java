7731cb8fb0fbaa8c08d3f69ffa14e091dcab2d95
Jul <lnacles@naver.com>
2023-05-11 16:32:08 +0900

    Fix:batch 서버 시간대 Asia/Seoul로 변경

--- batch/src/main/java/com/example/batch/BatchApplication.java
+++ batch/src/main/java/com/example/batch/BatchApplication.java
@@ -6,10 +6,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.Locale;
+import java.util.TimeZone;
+
 @EnableScheduling
 @EnableBatchProcessing
 @SpringBootApplication
 public class BatchApplication {
+	@PostConstruct
+	public void started() {
+		Locale.setDefault(Locale.KOREA);
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BatchApplication.class, args);
