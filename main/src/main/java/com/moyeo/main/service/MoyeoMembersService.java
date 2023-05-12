44b10a74d01828a4d6c496ab67f5d99582799d69
leejiyeon <sky75013@naver.com>
2023-05-12 09:32:09 +0900

    [BE] feat: 초대할 때 리스트로 req받기 수정

--- main/src/main/java/com/moyeo/main/service/MoyeoMembersService.java
+++ main/src/main/java/com/moyeo/main/service/MoyeoMembersService.java
@@ -7,7 +7,7 @@ import com.moyeo.main.dto.RegistMoyeoRes;
 import com.moyeo.main.entity.User;
 
 public interface MoyeoMembersService {
-    public void inviteMoyeoMembers(User user, List<MoyeoMembersReq> moyeoMembersReqList) throws Exception;
+    public Boolean inviteMoyeoMembers(User user, List<MoyeoMembersReq> moyeoMembersReqList) throws Exception;
     public RegistMoyeoRes registMoyeoMembers(User user, Long moyeoTimelineId) throws Exception;
     public Boolean updateMoyeoMembers(User user, Long moyeoTimelineId) throws Exception;
 
