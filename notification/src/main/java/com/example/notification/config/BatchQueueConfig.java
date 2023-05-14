3361e011b557eb271c3377a804f26814084fd1e8
Jul <lnacles@naver.com>
2023-05-14 21:13:39 +0900

    Feat:Publisher 세팅

---
+++ notification/src/main/java/com/example/notification/config/BatchQueueConfig.java
@@ -0,0 +1,43 @@
+package com.example.notification.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.converter.MessageConverter;
+
+public class BatchQueueConfig {
+    @Value("${batch.exchange}")
+    private static String EXCHANGE_NAME;
+    @Value("${batch.queue}")
+    private static String QUEUE_NAME;
+    @Value("${batch.route-key}")
+    private static String ROUTING_KEY;
+
+    @Bean
+    TopicExchange exchange(){
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    Queue queue(){
+        return new Queue(QUEUE_NAME);
+    }
+
+    @Bean
+    Binding binding(Queue queue,TopicExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    RabbitTemplate rabbitTemplate (ConnectionFactory connectionFactory, MessageConverter messageConverter){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+}
