27ebd61c7c078d63e521d1a31a09547c8c38b799
Jul <lnacles@naver.com>
2023-05-14 16:56:46 +0900

    Feat:Consumer init

---
+++ consumer/src/main/java/com/example/consumer/Config/MessageListener.java
@@ -0,0 +1,15 @@
+package com.example.consumer.Config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class MessageListener {
+    @RabbitListener(queues = "sample.queue")
+    public void receiveMessage(Message message){
+        log.info("Mesaage info : {}",message);
+    }
+}
