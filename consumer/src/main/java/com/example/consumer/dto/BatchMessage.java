6dfbc44aeda55b0a9fbe17571c5ec4005234ea25
Jul <lnacles@naver.com>
2023-05-15 18:00:40 +0900

    Done: 객체 연결완료

--- consumer/src/main/java/com/example/consumer/dto/BatchMessage.java
+++ consumer/src/main/java/com/example/consumer/dto/BatchMessage.java
@@ -1,19 +1,17 @@
 package com.example.consumer.dto;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import java.io.Serializable;
-@Builder
+
+
+@NoArgsConstructor
 @Data
 public class BatchMessage {
 //    private static final long serialVersionUID = 2349683458261238690L;
     private long id;
     private String message;
     private String deviceToken;
-    public BatchMessage(long id,String message ,String deviceToken){
-        this.id = id;
-        this.message = message;
-    }
+
 }
 
