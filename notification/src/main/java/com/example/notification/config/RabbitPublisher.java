4b85e28dc715d252a3721d35a4f9c948986fd459
woojin <woojin1home@naver.com>
2023-05-15 16:12:48 +0900

    [BE] fix : listener 작업중3

--- notification/src/main/java/com/example/notification/config/RabbitPublisher.java
+++ notification/src/main/java/com/example/notification/config/RabbitPublisher.java
@@ -9,7 +9,9 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RabbitPublisher {
 
     private static final String EXCHANGE_NAME = "sample.exchange";
@@ -34,9 +36,13 @@ public class RabbitPublisher {
     @Bean
     RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
         return rabbitTemplate;
     }
 
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
 
 }
