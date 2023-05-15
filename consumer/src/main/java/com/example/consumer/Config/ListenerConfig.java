ade7ac30ae35b8cc1b53ac385aa5777cfb1b3b0d
Jul <lnacles@naver.com>
2023-05-15 12:56:31 +0900

    Feat:AMPQ 프로토콜 MESSAGE READ하기

---
+++ consumer/src/main/java/com/example/consumer/Config/ListenerConfig.java
@@ -0,0 +1,43 @@
+package com.example.consumer.Config;
+
+import com.example.consumer.dto.BatchMessage;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.support.converter.MessageConversionException;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+@Configuration
+public class ListenerConfig {
+    @Bean
+    public RabbitListenerContainerFactory<?> rabbitListenerContainerFactory(ConnectionFactory connectionFactory){
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new MessageConverter() {
+            @Override
+            public Message toMessage(Object o, MessageProperties messageProperties) throws MessageConversionException {
+                return null;
+            }
+
+            @Override
+            public Object fromMessage(Message message) throws MessageConversionException {
+                try (ObjectInputStream ois  = new ObjectInputStream((new ByteArrayInputStream(message.getBody())))){
+                    return (BatchMessage) ois.readObject();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        });
+        return factory;
+    }
+}
