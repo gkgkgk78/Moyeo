c8efd37d63642e6764b85b67bf6325494427fc20
조은비 <immt12478@naver.com>
2023-05-15 10:25:41 +0900

    [FE] feat: home userinfo 위젯

--- frontend/lib/views/home_userinfo.dart
+++ frontend/lib/views/home_userinfo.dart
@@ -17,7 +17,40 @@ class HomeUserInfo extends StatelessWidget{
         height: 100,
         width: 300,
         // 유저의 상태를 표시해주는 (여행중/ 휴식중 등등) 혹은 모여를 시작한지 몇일째 이런거 넣어보기
-        child: Container(),
+        child: Row(
+          children: [
+            // 프로필 이미지
+            Container(
+              width: 100,
+            ),
+            // 사용자 정보
+            Container(
+              width: 200,
+              child: Column(
+                children: [
+                  // 나의 상태 표시 ( 여행중)
+                  Container(
+                    height: 50,
+                    child: Text(
+                      appViewModel.userInfo.timeLineId != -1
+                      ? appViewModel.userInfo.nowMoyeo
+                        ? "모여 여행중"
+                        : "여행중"
+                      : "휴식중"
+                    ),
+                  ),
+                  // 모여와 함께한지 몇일 이라던가 데이터 표시할 곳
+                  Container(
+                    height: 50,
+                    child: Text(
+                      "모여와 함께 한지 "
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )
       );
     }
     );
