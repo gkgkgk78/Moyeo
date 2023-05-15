468094f9e18f22718f76d2b95998c439cb94ea9f
Jul <lnacles@naver.com>
2023-05-15 14:09:22 +0900

    Fix:Message 객체 확인해보기

--- consumer/src/main/java/com/example/consumer/Config/MessageListener.java
+++ consumer/src/main/java/com/example/consumer/Config/MessageListener.java
@@ -16,6 +16,8 @@ public class MessageListener {
 
     @RabbitListener(queues = "batch.queue")
     public void receiveMessageFromBatch(Message message){
-        log.info("ReceiveMessageFromBatch Server info : {}",message);
+        log.info("ReceiveMessageFromBatch Server message info : {}",message);
+        log.info("ReceiveMessageFromBatch Server message.getMessageProperties() info : {}",message.getMessageProperties());
+        log.info("ReceiveMessageFromBatch Server message.getBody() info : {}",message.getBody());
     }
 }
