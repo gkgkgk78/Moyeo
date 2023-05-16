5a601cd2a39e7d6262c5044dbaf82c6af3182b97
woojin <woojin1home@naver.com>
2023-05-16 15:41:00 +0900

    [BE] fix : consumer 에 postinsert 관련 처리 로직 추가함

--- notification/src/main/java/com/example/notification/config/MessageListener.java
+++ notification/src/main/java/com/example/notification/config/MessageListener.java
@@ -3,23 +3,30 @@ package com.example.notification.config;
 
 import com.example.notification.dto.BatchMessage;
 import com.example.notification.dto.PostInsertReq;
+import com.example.notification.service.PostInsertAutogpt;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 @RabbitListener(queues = {"sample.queue"})
 public class MessageListener {
 
+
+    private final PostInsertAutogpt postInsertAutogpt;
+
+
+
     @RabbitHandler
     public void receiveMessage(PostInsertReq post) {
-        PostInsertReq r = post;
-
-        System.out.println("받음");
-        log.info(post.toString());
-        log.info("Mesaage info : {}", "받음");
+        log.info("비동기 post insert 후 autogpt작업 시작");
+        postInsertAutogpt.insert(post);
+        log.info("비동기 post insert 후 autogpt작업 완료");
     }
 
     @RabbitHandler
