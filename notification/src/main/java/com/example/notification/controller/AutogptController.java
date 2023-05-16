3d499cd7868c3fdf7ecf704078e1da92dabad962
woojin <woojin1home@naver.com>
2023-05-16 15:48:12 +0900

    [BE] fix : consumer 에 postinsert 관련 처리 로직 추가함1

--- notification/src/main/java/com/example/notification/controller/AutogptController.java
+++ notification/src/main/java/com/example/notification/controller/AutogptController.java
@@ -1,35 +1,30 @@
-package com.example.notification.controller;
-
-
-import com.example.notification.service.FcmService;
-import com.example.notification.service.FcmServiceImpl;
-import com.example.notification.service.PostInsertAutogpt;
-import com.example.notification.dto.PostInsertReq;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RequestMapping("/autogpt")
-@RequiredArgsConstructor
-@RestController
-@Slf4j
-public class AutogptController {
-
-
-
-    private final PostInsertAutogpt postInsertAutogpt;
-
-    @PostMapping(value = "")
-    public void getFavoritePostList(@ModelAttribute PostInsertReq req) throws Exception {
-        log.info("비동기 post insert 후 autogpt작업 시작");
-        postInsertAutogpt.insert(req);
-        log.info("비동기 post insert 후 autogpt작업 완료");
-    }
-
-
-
-}
+//package com.example.notification.controller;
+//
+//
+//import com.example.notification.dto.PostInsertReq;
+//import com.example.notification.service.PostInsertAutogpt;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RequestMapping("/autogpt")
+//@RequiredArgsConstructor
+//@RestController
+//@Slf4j
+//public class AutogptController {
+//
+//
+//    private final PostInsertAutogpt postInsertAutogpt;
+//
+//    @PostMapping(value = "")
+//    public void getFavoritePostList(@ModelAttribute PostInsertReq req) throws Exception {
+//        log.info("비동기 post insert 후 autogpt작업 시작");
+//        postInsertAutogpt.insert(req);
+//        log.info("비동기 post insert 후 autogpt작업 완료");
+//    }
+//
+//
+//}
