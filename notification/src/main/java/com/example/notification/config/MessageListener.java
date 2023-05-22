bb06bdc047934df206e980dbc5f08e37c909f114
woojin <woojin1home@naver.com>
2023-05-22 12:34:20 +0900

    [BE] fix : notification 추가 작업진행중2

--- notification/src/main/java/com/example/notification/config/MessageListener.java
+++ notification/src/main/java/com/example/notification/config/MessageListener.java
@@ -37,6 +37,7 @@ public class MessageListener {
     @RabbitHandler
     public void receiveBatchMessage(BatchMessage batchMessage) throws Exception {
         log.info("From Batch to Autogpt start");
+        log.info("message: 1로 들어옴");
         log.info("Message : {}", batchMessage);
         batchAutogpt.insert(batchMessage);
         log.info("From Batch to Autogpt end");
