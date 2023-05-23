5ffe286ecfeaadb0e2690f25c5a19e0acde9db29
woojin <woojin1home@naver.com>
2023-05-23 13:30:59 +0900

    [BE] fix : listener 작업 수정중

--- notification/src/main/java/com/example/notification/config/MessageListenerSecond.java
+++ notification/src/main/java/com/example/notification/config/MessageListenerSecond.java
@@ -1,53 +1,53 @@
-package com.example.notification.config;
-
-
-import com.example.notification.service.BatchAutogpt;
-import com.example.notification.service.PostInsertAutogpt;
-import com.example.sender.DTO.BatchMessage;
-import com.example.sender.DTO.PostInsertReq;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
-
-
-@Slf4j
-@Component
-@RequiredArgsConstructor
-@RabbitListener(queues = "sample.queue")
-public class MessageListenerSecond {
-
-
-    private final PostInsertAutogpt postInsertAutogpt;
-    private final BatchAutogpt batchAutogpt;
-
-
-    @RabbitHandler
-    public void receiveMessage(PostInsertReq post) {
-        log.info("비동기 post insert 후 autogpt작업 시작");
-        log.info("message: 2로 들어오고 끝냄");
-        //postInsertAutogpt.insert(post);
-
-        //log.info(post.toString());
-
-        log.info("비동기 post insert 후 autogpt작업 완료");
-
-    }
-
-    @RabbitHandler
-    public void receiveBatchMessage(BatchMessage batchMessage) throws Exception {
-        log.info("From Batch to Autogpt start");
-        log.info("message: 1로 들어옴");
-        log.info("Message : {}", batchMessage);
-        batchAutogpt.insertSecond(batchMessage);
-        log.info("From Batch to Autogpt end");
-    }
-
-    @RabbitHandler(isDefault = true)
-    public void handleDefault(Object message) {
-        log.info("invoice paid : {} ", message.toString());
-    }
-
-
-}
+//package com.example.notification.config;
+//
+//
+//import com.example.notification.service.BatchAutogpt;
+//import com.example.notification.service.PostInsertAutogpt;
+//import com.example.sender.DTO.BatchMessage;
+//import com.example.sender.DTO.PostInsertReq;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.stereotype.Component;
+//
+//
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//@RabbitListener(queues = "sample.queue")
+//public class MessageListenerSecond {
+//
+//
+//    private final PostInsertAutogpt postInsertAutogpt;
+//    private final BatchAutogpt batchAutogpt;
+//
+//
+//    @RabbitHandler
+//    public void receiveMessage(PostInsertReq post) {
+//        log.info("비동기 post insert 후 autogpt작업 시작");
+//        log.info("message: 2로 들어오고 끝냄");
+//        //postInsertAutogpt.insert(post);
+//
+//        //log.info(post.toString());
+//
+//        log.info("비동기 post insert 후 autogpt작업 완료");
+//
+//    }
+//
+//    @RabbitHandler
+//    public void receiveBatchMessage(BatchMessage batchMessage) throws Exception {
+//        log.info("From Batch to Autogpt start");
+//        log.info("message: 1로 들어옴");
+//        log.info("Message : {}", batchMessage);
+//        batchAutogpt.insertSecond(batchMessage);
+//        log.info("From Batch to Autogpt end");
+//    }
+//
+//    @RabbitHandler(isDefault = true)
+//    public void handleDefault(Object message) {
+//        log.info("invoice paid : {} ", message.toString());
+//    }
+//
+//
+//}
